dea098164fea2ecb7d2cd257cd8d2a38b40b9946
Andrew Sparish <Andrei.sp.93@gmail.com>
2024-05-31 09:53:23 +0300

    fix: fix data obj, change the keys according doc

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ function createData (inputs)  {
         "e2257e4191a9ea1832373c9a226de12474612b5a": "", 
         "7aa77765f15e1262b9a81a37868e144259d3e8c0": "",
         "f0065d20afb083c0b1388759c8018fb36da10c27": "",
-            job_type: "",    
-            job_source: "",
-            job_description: "",
-            address: "",
-            city: "",
-            state: "",
-            zip_code: "",  
-            area: "",
-            date: "",
-            start_time: "",  
-            end_time: "",    
-            test_select: ""
+        "bb8ac84cf79e922fcefc68b7a4eac1a7b3158eb7":"",    
+        "f733d0cc581c93dbb4292551ee380c3a46ed7f9c": "",
+        "58950237b019d5a169f18a3c2effe315df2fea33": "",
+        "e6aecd97538377e2d9e849296d8ebe276193ba98": "",
+        "fae3068fdd7ea4c21b3af69eb43fce5e10504c32": "",
+        "624fe041e7341fea03e8784f884e2bee3098a2c2": "",
+        "fee67be6b7c7577b95b14dfa21e3ed9fc77437db": "",  
+        "d2a559b6f18c96fee7ff0f632998684776cd53eb": "",
+        "c58eb311eebc13c206ec33d32c0139f0919df32a": "",
+        "d498fa5cefb771b39ded47b3268011a1624c9c3b": "",  
+        "cc7fb0f152797c26771f5c7da6f66e49043e2a1c": "",    
+        "7231c536ea25c94f09f70bcaa9e288ba91557008": ""
     };
 
     let i = 0;
